b5aa9e376a1da29173cb26748218e7f0e9e9a9ce
suginohiroya <h-sugino@tamaishiki.com>
2025-04-15 09:08:38 +0900

    修正１

--- rename.sh
+++ rename.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
-# ファイル名リストを読み込む
+# 変更前と変更後のファイル名のリストを読み込む
 source old_names.sh
 source new_names.sh
 
-# 配列の長さを取得
-length=${#old_names[@]}
+# 変更元のディレクトリを指定
+source_dir="old_name_files"
 
-# 出力ディレクトリの作成
-output_dir="./renamed_files"
+# 新しい保存先ディレクトリを指定
+destination_dir="new_files"
 
-# 各ファイルに対してリネームを実行
-for ((i=0; i<length; i++)); do
-    old_name="${old_names[$i]}"
-    new_name="${new_names[$i]}"
+# 各ファイルを逆順でリネーム
+for (( i=${#old_names[@]}-1; i>=0; i-- )); do
 
-    # studyディレクトリ内でファイルを検索
-    found_file=$(find ./study -name "$old_name" -type f)
-
-    if [ -n "$found_file" ]; then
-        echo "リネーム: $old_name -> $new_name"
-        # ファイルを新しいディレクトリにコピーしてリネーム
-        cp "$found_file" "$output_dir/$new_name"
+    old_name="${source_dir}/${old_names[$i]}"
+    new_name="${destination_dir}/${new_names[$i]}"
+    if [ -f "$old_name" ]; then
+        mv "$old_name" "$new_name"
+        echo "成功: $new_name"
     else
-        echo "警告: $old_name が見つかりません"
+        echo "警告: $old_name not found"
     fi
 done
-
-echo "リネーム処理が完了しました"
